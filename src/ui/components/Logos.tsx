1f37246254cbcacdc016d58790710129c940002b
Ademola Raimi <ademolaraimi.nig@gmail.com>
2024-01-24 21:51:05 +0100

    logo style updated

--- src/ui/components/Logos.tsx
+++ src/ui/components/Logos.tsx
@@ -1,45 +1,55 @@
-import React from "react";
-import { Box, Typography } from "@mui/material";
-import styled from "@emotion/styled";
+import React from 'react';
+import { Box, Grid, Typography } from '@mui/material';
+import styled from '@emotion/styled';
 
-const StyledRow = styled(Box)({
-  display: "inline-flex",
-  alignItems: "center",
-  justifyContent: "center",
-  gap: "30px",
-  padding: "0",
-  paddingTop: "50px",
-  position: "relative",
+const StyledRow = styled(Grid)({
+  maxWidth: '1054px',
+  width: '100%',
+  margin: '0 auto',
+  gap: '30px',
+  padding: '50px 0',
+  alignItems: 'center',
+  justifyContent: 'center',
+  '@media screen and (max-width: 991px)': {
+    textAlign: 'center'
+  }
 });
 
-const StyledColumn = styled(Box)({
-  display: "flex",
-  flexDirection: "column",
-  width: "auto",
-  alignItems: "center",
-  position: "relative",
-});
-
-const StyledImage = styled("img")({
-  width: "auto",
-  height: "auto",
+const StyledImage = styled('img')({
+  width: 'auto',
+  height: 'auto',
 });
 
 const Logos = (): JSX.Element => {
   return (
-    <StyledRow>
-      <StyledColumn width="153px">
-        <StyledImage alt="Fa brands" src="/icons/fa-brands-1.svg" width="103px" height="34px" />
-      </StyledColumn>
-      <StyledColumn width="146px">
-        <StyledImage alt="Fa brands" src="/icons/fa-brands-2.svg" width="83px" height="59px" />
-      </StyledColumn>
-      <StyledColumn width="152px">
-        <StyledImage alt="Fa brands" src="/icons/fa-brands-3.svg" width="102px" height="75px" />
-      </StyledColumn>
-      <StyledImage alt="Col md" src="/icons/fa-brands-4.svg" width="151px" />
-      <StyledImage alt="Col md" src="/icons/fa-brands-5.svg" width="151px" />
-      <StyledImage alt="Col md" src="/icons/fa-brands-6.svg" width="149px" />
+    <StyledRow container className="center">
+      <Grid item xs={12} sm={4} md={4} lg={1.6}>
+        <StyledImage
+          alt="Fa brands"
+          src="/icons/fa-brands-1.svg"
+        />
+      </Grid>
+      <Grid item xs={12} sm={4} md={4} lg={1.6}>
+        <StyledImage
+          alt="Fa brands"
+          src="/icons/fa-brands-2.svg"
+        />
+      </Grid>
+      <Grid item xs={12} sm={4} md={4} lg={1.6}>
+        <StyledImage
+          alt="Fa brands"
+          src="/icons/fa-brands-3.svg"
+        />
+      </Grid>
+      <Grid item xs={12} sm={4} md={4} lg={1.6}>
+        <StyledImage alt="Col md" src="/icons/fa-brands-4.svg" />
+      </Grid>
+      <Grid item xs={12} sm={4} md={4} lg={1.6}>
+        <StyledImage alt="Col md" src="/icons/fa-brands-5.svg" />
+      </Grid>
+      <Grid item xs={12} sm={4} md={4} lg={1.6}>
+        <StyledImage alt="Col md" src="/icons/fa-brands-6.svg" />
+      </Grid>
     </StyledRow>
   );
 };
