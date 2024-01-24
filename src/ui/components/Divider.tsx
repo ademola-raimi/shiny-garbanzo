d04a5343cee1515ea17aa53346cc6c9308c3578a
Ademola Raimi <ademolaraimi.nig@gmail.com>
2024-01-24 18:40:42 +0100

    update divider

--- src/ui/components/Divider.tsx
+++ src/ui/components/Divider.tsx
@@ -1,70 +1,64 @@
-import React from "react";
-import { Box, Typography } from "@mui/material";
-import styled from "@emotion/styled";
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import styled from '@emotion/styled';
 
-const StyledNavbar = styled(Box)({
-  position: "relative",
-  width: "1440px",
-  height: "91px",
-  background: "white",
-});
-
-const StyledSection = styled(Box)({
-  position: "absolute",
+const StyledNavbar = styled('div')({
+  maxWidth: '1050px',
+  width: '100%',
+  margin: '0 auto',
+  borderBottom: '1px solid #ECECEC',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  '@media screen and (max-width: 767px)': {
+    display: 'none',
+  },
 });
 
 const StyledLink = styled(Typography)({
   fontFamily: "'Montserrat', sans-serif",
-  fontWeight: "600",
-  fontSize: "14px",
-  color: "text-secondary-text-color",
-  textAlign: "center",
-  letterSpacing: "0.20px",
-  lineHeight: "24px",
-  whiteSpace: "nowrap",
-});
-
-const Line = styled('hr')({
-  background: "#ECECEC",
-  width: "1049px",
-  height: "1px",
+  fontWeight: '600',
+  fontSize: '14px',
+  color: '#737373',
+  textAlign: 'center',
+  letterSpacing: '0.20px',
+  lineHeight: '24px',
+  whiteSpace: 'nowrap',
 });
 
 const Divider = (): JSX.Element => {
   return (
-    <>
     <StyledNavbar>
-      <StyledSection width="1051px" height="72px" top="10px" left="193px">
-        <StyledSection width="134px" height="72px" top="0" left="280px">
-          <Box display="flex" alignItems="center" justifyContent="center" gap="8px" p="24px" position="relative">
-            <StyledLink>
-              Description
-            </StyledLink>
-          </Box>
-        </StyledSection>
-        <StyledSection width="355px" height="72px" top="0" left="414px">
-          <StyledSection width="220px" height="72px" top="0" left="0">
-            <Box display="flex" alignItems="center" justifyContent="center" gap="8px" p="24px" position="relative">
-              <StyledLink>
-                Additional Information
-              </StyledLink>
-            </Box>
-          </StyledSection>
-          <StyledSection width="138px" height="72px" top="0" left="217px">
-            <Box display="flex" alignItems="center" justifyContent="center" gap="8px" p="24px" position="relative">
-              <StyledLink>
-                Reviews
-              </StyledLink>
-              <StyledLink color="text-secondary-color-1">
-                (0)
-              </StyledLink>
-            </Box>
-          </StyledSection>
-        </StyledSection>
-      </StyledSection>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        gap="8px"
+        p="24px"
+        position="relative"
+      >
+        <StyledLink>Description</StyledLink>
+      </Box>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        gap="8px"
+        p="24px"
+      >
+        <StyledLink>Additional Information</StyledLink>
+      </Box>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        gap="8px"
+        p="24px"
+      >
+        <StyledLink>Reviews</StyledLink>
+        <StyledLink color="text-secondary-color-1">(0)</StyledLink>
+      </Box>
     </StyledNavbar>
-    <Line/>
-    </>
   );
 };
 
